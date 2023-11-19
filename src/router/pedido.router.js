b195a4daeec12464b3e2dbb5cc67d58deab68fee
Ana Paula Santos <santosilva.anapaula@gmail.com>
2023-11-19 12:00:14 -0300

    Fix: correcao de validacao

--- src/router/pedido.router.js
+++ src/router/pedido.router.js
@@ -3,13 +3,13 @@ const router = require("express").Router();
 const pedidoController = require("../controller/pedido.controller");
 
 const authMiddleware = require ("../middleware/auth.middleware");
-const { validaPedido, validaIdParams, validaProdutosCarrinhoPedido } = require("../middleware/validacao.middleware");
+const { validaPedido, validaIdParams, validaPizzasCarrinhoPedido } = require("../middleware/validacao.middleware");
 const paginacao = require("../middleware/paginacao.middleware");
 
 router.get("/findById/:id", authMiddleware, validaIdParams, pedidoController.findPedidoByIdController);
 router.get("/findAll", authMiddleware, paginacao, pedidoController.findAllPedidoController);
 
-router.post("/create", authMiddleware, validaProdutosCarrinhoPedido, validaPedido, pedidoController.createPedidoController);
+router.post("/create", authMiddleware, validaPizzasCarrinhoPedido, validaPedido, pedidoController.createPedidoController);
 
 router.delete("/delete/:id", authMiddleware, validaIdParams, pedidoController.deletePedidoController);
 
