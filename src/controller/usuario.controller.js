04589a8571c8d42ef932d84e002a3d5eb62381c5
Ana Paula Santos <santosilva.anapaula@gmail.com>
2023-11-19 11:58:52 -0300

    Fix: ajustando requisicao do body

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -32,7 +32,7 @@ const findAllUsersController = async (req, res) => {
 
 const createUserController = async (req, res) => {
     try {
-        return res.status(201).send(await userService.createUserService(body));
+        return res.status(201).send(await userService.createUserService(req.body));
 
     } catch (err) {
         console.log(`erro:${err.message}`);
@@ -42,7 +42,7 @@ const createUserController = async (req, res) => {
 
 const updateUserController = async (req, res) => {
     try {
-        return res.send(await userService.updateUserService(req.params.id, body));
+        return res.send(await userService.updateUserService(req.params.id, req.body));
         
     } catch (err) {
         console.log(`erro:${err.message}`);
